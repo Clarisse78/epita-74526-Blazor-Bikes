3488a0a9802a96edb9b889b9a28483672378934b
Lucile Pelou <Clarisse787878@gmail.com>
2024-02-13 11:33:23 +0000

    Bikes

---
+++ epita-74526-Blazor-Bikes/Components/BikesStationsDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace test.models
+{
+    public class Stations
+    {
+        public int number { get; set; }
+        public string contract_name { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        //public Position position { get; set; }
+        public bool banking { get; set; }
+        public bool bonus { get; set; }
+        public int bike_stands { get; set; }
+        public int available_bike_stands { get; set; }
+        public int available_bikes { get; set; }
+        public string status { get; set; }
+        public long last_update { get; set; }
+    }
+
+    public  static class AllStations {
+        private static Stations[]? _stations;
+
+        public static void setStations(Stations[] forecasts)
+        {
+            _stations = forecasts;
+        }
+        public static Stations[]? getStations()
+        {
+            return _stations;
+        }
+    }
+    
+}
